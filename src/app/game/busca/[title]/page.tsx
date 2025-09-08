ee6b9e9fc552a0d41aa8a65dc1e0beae98efceb1
devsalomao <christophersalomao5@gmail.com>
2025-09-08 14:47:03 -0300

    feat: busca e exibicao de jogos   e paginade busca finalizadas

---
+++ src/app/game/busca/[title]/page.tsx
@@ -0,0 +1,48 @@
+import { Container } from "@/components/container";
+import { GameCard } from "@/components/gameCard";
+import { SearchInput } from "@/components/searchInput";
+import type { GameProps } from "@/utils/types/game";
+
+async function getData(title: string) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game&title=${title}`,
+      { next: { revalidate: 320 } }
+    );
+
+    return response.json();
+  } catch (error) {
+    throw new Error("Failed to fetch data");
+  }
+}
+
+export default async function Search({
+  params: { title },
+}: {
+  params: { title: string };
+}) {
+  const games: GameProps[] | null = await getData(title);
+
+  console.log(games);
+
+  return (
+    <main className="w-full">
+      <Container>
+        <SearchInput />
+        <h1 className="font-bold text-xl mt-8 mb-5">
+          Veja o que encontramos em nossa base
+        </h1>
+
+        {!games ? (
+          <p>Não encontramos nenhum jogo com esse título</p>
+        ) : (
+          <section className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {games.map((game) => (
+              <GameCard key={game.id} data={game} />
+            ))}
+          </section>
+        )}
+      </Container>
+    </main>
+  );
+}
